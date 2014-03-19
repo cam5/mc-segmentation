5133161c97379eb599bb809a8628ad11428d9bd3
Cameron Hurd <cam.hurd@gmail.com>
2014-03-19 16:07:13 -0400

    dropping in a hook for woocommerce orders

--- mc-segmentation.php
+++ mc-segmentation.php
@@ -68,12 +68,69 @@ function mc_woocommerce_custom_fields() {
             'label' => 'Mailchimp Options'
         ) );
 
-        x_add_metadata_field('x_fieldName1', 'product', array(
+        x_add_metadata_field('record_purchase', 'product', array(
             'group' => 'mcList',
             'field_type' => 'checkbox',
             'label' => 'Record Purchase of this product?',
             'display_column' => true
         ));
+    }
+}
+
+add_action( 'woocommerce_payment_complete' , 'woo_mc_segmentation_complete_order' );
+add_action( 'woocommerce_order_status_completed' , 'woo_mc_segmentation_complete_order' );
+
+function woo_mc_segmentation_complete_order( $order_id = 0 )
+{
+
+    if ( 0 < $order_id )
+    {
+        // Get order object
+        $order = new WC_Order( $order_id );
+
+        // Run through each product ordered
+        $items = $order->get_items();
+
+        // Placeholder for what we're taking down.
+        $items_to_record = array();
+
+        if (sizeof($items)>0)
+            foreach($items as $item) {
+                if (isset($item['variation_id']) && $item['variation_id'] > 0)
+                    $_product = new WC_Product_Variation( $item['variation_id'] );
+                else
+                    $_product = new WC_Product( $item['product_id'] );
+
+                if (get_post_meta($_product->get_post_data()->ID, 'record_purchase', true))
+                    $items_to_record[] = $_product->get_sku();
+            }
+
+        include( plugin_dir_path( __FILE__ ) . '/MailChimpApi.php' );
+
+        $api = new \Drewm\MailChimp(get_option('mc_segmentation')['apikey']);
+
+        $merge_vars = array(
+            'EMAIL' => 'mc_purchas_test_11dg@cameronhurd.com',
+            'FNAME' => $order->billing_first_name,
+            'LNAME' => $order->billing_last_name
+        );
+
+        if (sizeof($items_to_record)>0)
+            $merge_vars['PURCHASED'] = implode(', ', $items_to_record);
+
+        $list_id = "37d5137c62";
+        
+        $result = $api->call('lists/subscribe', array(
+            'id'                => $list_id,
+            'email'             => array( 'email' => $merge_vars['EMAIL'] ),
+            'merge_vars'        => $merge_vars,
+            'double_optin'      => true,
+            'update_existing'   => true,
+            'replace_interests' => false,
+            'send_welcome'      => true
+            ) 
+        );
+        
     }
 }
 
